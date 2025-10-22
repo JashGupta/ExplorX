17873f04bd829565192c50fe8a8e2afa74f8c329
JashGupta <jashgupta77@gmail.com>
2025-10-22 20:18:12 +0530

    added more params for registering a hotel

--- Backend/controllers/hotelController.js
+++ Backend/controllers/hotelController.js
@@ -3,15 +3,24 @@ import User from "../models/userModel.js";
 
 export const registerHotel = async (req, res) => {
     try {
-        const { name, address, contact, city } = req.body;
         const owner = req.user._id;
 
         const hotel = await Hotel.findOne({owner});
         if(hotel) {
             return res.json({success: false, message: "Hotel already registered"});
         }
+        
+        const hotelData = {
+            ...req.body,
+            owner,
+            rooms: [],
+            host: {
+                name: req.user.username,
+                profilePic: req.user.image || "",
+            },
+        };
 
-        await Hotel.create({ name, address, contact, owner, city });
+        await Hotel.create(hotelData);
         await User.findByIdAndUpdate(owner, { role: "hotelOwner" });
 
         return res.json({success: true, message: "Hotel registered successfully"});
--- Backend/models/hotelModel.js
+++ Backend/models/hotelModel.js
@@ -1,12 +1,27 @@
 import mongoose from "mongoose";
 
-const hotelSchema = new mongoose.Schema({
+const hotelSchema = new mongoose.Schema(
+  {
     name: { type: String, required: true },
     address: { type: String, required: true },
     contact: { type: String, required: true },
-    owner: { type: String, required: true },
-    city: { type: String, required: true },
-}, {timeStamps: true});
+    city: { type: String, required: true, index: true },
+    startingPrice: { type: Number, required: false },
+    rating: { type: Number, min: 0, max: 5, default: 0 },
+    reviews: { type: Number, default: 0 },
+    images: [String],
+    amenities: [String],
+    policies: [String],
+    offer: { type: String },
+    owner: { type: String, ref: "User" },
+    rooms: { type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Room" }], default: [] },
+    host: {
+      name: String,
+      profilePic: String,
+    },
+  },
+  { timestamps: true }
+);
 
 const Hotel = mongoose.model("Hotel", hotelSchema);
 
