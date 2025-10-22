b63ce6514fd4c570256276f9e665b086a9acb4c5
JashGupta <jashgupta77@gmail.com>
2025-10-22 18:05:00 +0530

    .

--- Backend/routes/roomRouter.js
+++ Backend/routes/roomRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import upload from "../middlewares/uploadMiddleware.js";
 import { createRoom, getOwnerRooms, getRooms, toggleRoomAvailability } from "../controllers/roomController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 
 const roomRouter = express.Router();
 
-roomRouter.post('/', upload.array('images', 4), createRoom)
+roomRouter.post('/', upload.array('images', 4), authMiddleware, createRoom)
 roomRouter.get('/', getRooms);
-roomRouter.get('/owner', getOwnerRooms);
+roomRouter.get('/owner', authMiddleware, getOwnerRooms);
 roomRouter.post('/toggle-availability', toggleRoomAvailability);
 
 export default roomRouter;
